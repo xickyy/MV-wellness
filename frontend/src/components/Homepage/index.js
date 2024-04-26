ade53d2498b28c5932d1dce5f4a5c820cb77f7b9
ricky <ricky_moore@ymail.com>
2024-04-25 20:42:02 -0700

    troubbleshooting video for homepage

--- frontend/src/components/Homepage/index.js
+++ frontend/src/components/Homepage/index.js
@@ -1,10 +1,13 @@
 import './Homepage.css';
-
+import homepagevid from './homepagevid.mp4';
 
 const Homepage = () => {
   return (
     <div>
       <h2>Homepage</h2>
+      <video controls muted autoplay loop>
+        <source src={homepagevid}></source>
+      </video>
     </div>
   );
 }
