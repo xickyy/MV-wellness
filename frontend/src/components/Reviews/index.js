aff99d881635f5ef456daae0e9ce93a5024c3991
ricky <ricky_moore@ymail.com>
2024-05-03 23:04:08 -0700

    soo i need to make a server side in order to scrape the google reviews

--- frontend/src/components/Reviews/index.js
+++ frontend/src/components/Reviews/index.js
@@ -1,6 +1,6 @@
 import './Reviews.css';
 
-import GooglePlacesReviewsApi from '../GooglePlacesReviewsApi';
+
 
 const Reviews = () => {
   return (
@@ -9,7 +9,7 @@ const Reviews = () => {
       <h2>Reviews Page</h2>
 
       <div>
-        <GooglePlacesReviewsApi apiKey={'AIzaSyCjSXNAi8GFCToY1e9tlKg2GaYb8KUSf9U'} placeId={'ChIJ1ezppTpZwoARKabjrAlOv-A'} />
+
       </div>
 
     </div>
