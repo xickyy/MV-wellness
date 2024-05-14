fda2154f1f3bbefce90c52d92edc7fed1ddc4679
ricky <ricky_moore@ymail.com>
2024-05-13 19:19:49 -0700

    mens health page is done for desktop and mobile

--- frontend/src/components/Services/MensHealth/index.js
+++ frontend/src/components/Services/MensHealth/index.js
@@ -16,48 +16,41 @@ const MensHealth = () => {
           including but not limited to:
         </p>
 
-        <div className='flex w-full'>
-          <div>
-            <div className='flex justify-around px-4 md:px-0'>
-              <ul className='list-disc md:ml-24 py-8'>
-                <li>Liver disease</li>
-                <li>Lung disease</li>
-                <li>Colon cancer</li>
-                <li>Erectile Disfunction</li>
-                <li>high blood pressure</li>
-              </ul>
-              <ul className='list-disc md:ml-24 py-8' >
-                <li>stroke</li>
-                <li>heart disease</li>
-                <li>low testosterone</li>
-                <li>diabetes</li>
-                <li>prostate cancer</li>
-              </ul>
-            </div>
-
-            <div>
-              <p className='mb-4 indent-8'>
-                While some conditions become more prevalent with age, it's crucial to seek regular medical care irrespective of age.
-                Men aged 18-40 should undergo yearly physical exams, encompassing preventive screenings for high blood pressure, cholesterol, and diabetes, along with sexually transmitted disease checks
-                for sexually active individuals. Those over 40 years old should maintain annual exams, with frequency potentially increasing with age. Additional screenings like colonoscopies and prostate screening
-                checks may be recommended to monitor age-related conditions.
-              </p>
-
-              <p className='mb-4 indent-8'>
-                While genetics and family history play a large role in health, lifestyle choices have a significant influence over health. For example, lack of exercise, poor diet, and smoking, can escalate
-                the risk of serious health issues. Fortunately, we're here to help mitigate these risks through tailored lifestyle recommendations.
-              </p>
-            </div>
+          <div className='flex justify-around pl-4 md:px-0'>
+            <ul className='list-disc md:ml-24 py-8'>
+              <li>Liver disease</li>
+              <li>Lung disease</li>
+              <li>Colon cancer</li>
+              <li>Erectile Disfunction</li>
+              <li>high blood pressure</li>
+            </ul>
+            <ul className='list-disc md:ml-24 py-8' >
+              <li>stroke</li>
+              <li>heart disease</li>
+              <li>low testosterone</li>
+              <li>diabetes</li>
+              <li>prostate cancer</li>
+            </ul>
           </div>
 
-          <img className='hidden md:block mens-health-img' src={men} alt=''></img>
-
-        </div>
+        <img className='hidden md:block mens-health-img' src={men} alt=''></img>
 
         <img className='md:hidden mens-health-mobile-style' src={men} alt=''></img>
 
 
         <p className='mb-4 indent-8'>
+          While some conditions become more prevalent with age, it's crucial to seek regular medical care irrespective of age.
+          Men aged 18-40 should undergo yearly physical exams, encompassing preventive screenings for high blood pressure, cholesterol, and diabetes, along with sexually transmitted disease checks
+          for sexually active individuals. Those over 40 years old should maintain annual exams, with frequency potentially increasing with age. Additional screenings like colonoscopies and prostate screening
+          checks may be recommended to monitor age-related conditions.
+        </p>
+
+        <p className='mb-4 indent-8 custom-while-genetics'>
+          While genetics and family history play a large role in health, lifestyle choices have a significant influence over health. For example, lack of exercise, poor diet, and smoking, can escalate
+          the risk of serious health issues. Fortunately, we're here to help mitigate these risks through tailored lifestyle recommendations.
+        </p>
+
+        <p className='mb-4 indent-8 custom-we-collab'>
           We collaborate with you to devise a wellness program aligned with
           your lifestyle, addressing your specific health care needs. Recommendations may span initiating an exercise regimen, managing stress effectively,
           and cutting down on alcohol, tobacco use and or other harmful habits. Furthermore adopting a balanced diet rich in fruits, vegetables and lean proteins is beneficial for health promotion.
