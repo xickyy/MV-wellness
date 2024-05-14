7496de25462f5add051fe73cceff000d7a52b288
ricky <ricky_moore@ymail.com>
2024-05-14 16:32:28 -0700

    added womens health and std screening pages

---
+++ frontend/src/components/Services/StdScreen/index.js
@@ -0,0 +1,11 @@
+import './StdScreen.css';
+
+const StdScreen = () => {
+    return (
+        <div className='service-background-gradient'>
+            Std Screening Page
+        </div>
+    );
+};
+
+export default StdScreen;
---
+++ frontend/src/components/Services/WomensHealth/index.js
@@ -0,0 +1,11 @@
+import './WomensHealth.css';
+
+const WomensHealth = () => {
+    return (
+        <div className='service-background-gradient'>
+            Womens Health Page
+        </div>
+    );
+};
+
+export default WomensHealth;
